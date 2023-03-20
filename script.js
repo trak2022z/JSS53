c541a7cf0298203abc19eff39c655e692c2db587
Tom Krok <91782249+trak2022z@users.noreply.github.com>
2023-03-20 06:13:54 +0000

    1

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 function changeColor(newColor) {
   const elem = document.getElementById("para");
   elem.style.color = newColor;
+  
+  
+}
+
+function changeColor2(newColor2) {
+  const elem = document.querySelector(".myclass");
+  elem.style.color = newColor2;
+  
+}
+
+function changeColor3(newColor3) {
+  const elem = document.querySelectorAll("p.intro");
+
+  for (i = 0; i < elem.length; i++) {
+    elem[i].style.backgroundColor = newColor3;
+}
 }
